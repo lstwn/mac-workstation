63ad79296e0c8d9e6820226d20b85c63ce19075b
Leo <lstwn@mailbox.org>
2021-05-07 13:24:29 +0200

    work launch script

---
+++ work.fish
@@ -0,0 +1,21 @@
+#!/usr/local/bin/fish
+
+cd
+tmux rename-session "work"
+tmux rename-window "main"
+
+tmux new-window -d -n "levi dev"   "cd ~/Projects/levi/ && vim && fish"
+tmux new-window -d -n "levi run"   "cd ~/Projects/levi/ && docker compose up && fish"
+tmux split-window -v -t "levi run" "cd ~/Projects/levi/ && fish"
+tmux split-window -h -t "levi run" "cd ~/Projects/levi/ && fish"
+
+tmux new-window -d -n "lusi dev"   "cd ~/Projects/lusi/ && vim && fish"
+tmux new-window -d -n "lusi run"   "cd ~/Projects/lusi/ && docker compose up && fish"
+tmux split-window -v -t "lusi run" "cd ~/Projects/lusi/ && fish"
+tmux split-window -h -t "lusi run" "cd ~/Projects/lusi/ && fish"
+
+# tmux new-window -n "docker" "cd ~/Projects/lusi/ && fish"
+# tmux split-window -v "cd ~/Projects/lusi/ && fish"
+# tmux split-window "cd ~/Projects/levi/ && fish"
+# tmux split-window -v "cd ~/Projects/levi/ && fish"
+# tmux select-layout -t "docker" "tiled"
