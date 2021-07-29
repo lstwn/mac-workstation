a1fd07c340b7b5a092d2a518c3d0230a62e56c6a
Leo <lstwn@mailbox.org>
2021-07-29 09:19:57 +0200

    updates

--- work.fish
+++ work.fish
@@ -4,12 +4,12 @@ cd
 tmux rename-session "work"
 tmux rename-window "main"
 
-tmux new-window -d -n "levi dev"   "cd ~/Projects/levi/ && vim; fish"
+tmux new-window -d -n "levi dev"   "cd ~/Projects/levi/ && $EDITOR; fish"
 tmux new-window -d -n "levi run"   "cd ~/Projects/levi/ && docker compose up; fish"
 tmux split-window -v -t "levi run" "cd ~/Projects/levi/ && fish"
 tmux split-window -h -t "levi run" "cd ~/Projects/levi/ && fish"
 
-tmux new-window -d -n "lusi dev"   "cd ~/Projects/lusi/ && vim; fish"
+tmux new-window -d -n "lusi dev"   "cd ~/Projects/lusi/ && $EDITOR; fish"
 tmux new-window -d -n "lusi run"   "cd ~/Projects/lusi/ && docker compose up; fish"
 tmux split-window -v -t "lusi run" "cd ~/Projects/lusi/ && fish"
 tmux split-window -h -t "lusi run" "cd ~/Projects/lusi/ && fish"
