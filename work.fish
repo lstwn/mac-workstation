16ae0434622655d2a18c25df7f272065c04faa87
Leo <lstwn@mailbox.org>
2021-09-02 15:13:40 +0200

    work fish update

--- work.fish
+++ work.fish
@@ -5,13 +5,18 @@ tmux rename-session "work"
 tmux rename-window "main"
 
 tmux new-window -d -n "levi dev"   "cd ~/Projects/levi/ && $EDITOR; fish"
-tmux new-window -d -n "levi run"   "cd ~/Projects/levi/ && docker compose up; fish"
-tmux split-window -v -t "levi run" "cd ~/Projects/levi/ && fish"
-tmux split-window -h -t "levi run" "cd ~/Projects/levi/ && fish"
 
 tmux new-window -d -n "lusi dev"   "cd ~/Projects/lusi/ && $EDITOR; fish"
-tmux new-window -d -n "lusi run"   "cd ~/Projects/lusi/ && docker compose up; fish"
-tmux split-window -v -t "lusi run" "cd ~/Projects/lusi/ && fish"
-tmux split-window -h -t "lusi run" "cd ~/Projects/lusi/ && fish"
+
+tmux new-window   -d -n "services" "cd ~/Projects/lusi/services/cassandra && docker compose up; fish"
+tmux split-window -h -t "services" "cd ~/Projects/lusi/services/rabbitMQ  && docker compose up; fish"
+tmux split-window -h -t "services" "cd ~/Projects/lusi/services/minio     && docker compose up; fish"
+tmux split-window -v -t "services" "cd ~/Projects/lasi/                   && fish"
+tmux split-window -h -t "services" "cd ~/Projects/lusi/                   && fish"
+tmux split-window -h -t "services" "cd ~/Projects/levi/                   && fish"
+tmux split-window -v -t "services" "cd ~/Projects/lui/                    && fish"
+tmux split-window -h -t "services" "cd ~/Projects/                        && fish"
+tmux split-window -h -t "services" "cd ~/Projects/                        && fish"
+tmux select-layout   -t "services" tiled
 
 bw copy passwordtotp "Lavego Login"
