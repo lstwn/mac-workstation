40000f3828bdc2d7dbd4616dedfe2e7d17eec1ea
Leo <lstwn@mailbox.org>
2022-02-16 12:23:17 +0100

    open docker automatically and possibly switch to existing session

--- work.fish
+++ work.fish
@@ -1,15 +1,26 @@
 #!/usr/local/bin/fish
 
-tmux rename-session "work"
+set SESSION_NAME "work"
+set CURRENT_SESSION (tmux display-message -p '#S')
+
+if tmux switch-client -t "$SESSION_NAME" &> /dev/null
+  tmux kill-session -t "$CURRENT_SESSION"
+  exit 0
+end
+
+tmux rename-session "$SESSION_NAME"
 tmux rename-window "main"
 
-tmux new-window -d -n "levi dev"   "cd ~/Projects/levi/ && $EDITOR; fish"
+tmux new-window -d -n "levi dev"   "cd ~/Projects/levi/ && fish"
 
-tmux new-window -d -n "lusi dev"   "cd ~/Projects/lusi/ && $EDITOR; fish"
+tmux new-window -d -n "lusi dev"   "cd ~/Projects/lusi/ && fish"
 
-tmux new-window   -d -n "services" "cd ~/Projects/lusi/services/cassandra && docker compose up; fish"
-tmux split-window -h -t "services" "cd ~/Projects/lusi/services/rabbitMQ  && docker compose up; fish"
-tmux split-window -h -t "services" "cd ~/Projects/lusi/services/minio     && docker compose up; fish"
+# launch docker in the background
+open -g -j -a Docker
+
+tmux new-window   -d -n "services" "cd ~/Projects/lusi/services/cassandra && sleep 30 && docker compose up; fish"
+tmux split-window -h -t "services" "cd ~/Projects/lusi/services/rabbitMQ  && sleep 30 && docker compose up; fish"
+tmux split-window -h -t "services" "cd ~/Projects/lusi/services/minio     && sleep 30 && docker compose up; fish"
 tmux split-window -v -t "services" "cd ~/Projects/lasi/                   && fish"
 tmux split-window -h -t "services" "cd ~/Projects/lusi/                   && fish"
 tmux split-window -h -t "services" "cd ~/Projects/levi/                   && fish"
