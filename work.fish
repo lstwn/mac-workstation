9c0635386c7d33949a1419e3781c929a84373eb5
Leo <lstwn@mailbox.org>
2023-09-07 15:04:28 +0200

    updates

--- work.fish
+++ work.fish
@@ -2,6 +2,7 @@
 
 set SESSION_NAME "work"
 set CURRENT_SESSION (tmux display-message -p '#S')
+set SLEEP_COMMAND "sleep 50"
 
 if tmux switch-client -t "$SESSION_NAME" &> /dev/null
   tmux kill-session -t "$CURRENT_SESSION"
@@ -11,14 +12,8 @@ end
 tmux rename-session "$SESSION_NAME"
 tmux rename-window "main"
 
-tmux new-window -d -n "levi dev"   "cd ~/Projects/levi/ && fish"
-
-tmux new-window -d -n "lusi dev"   "cd ~/Projects/lusi/ && fish"
-
 # launch docker in the background
 open -g -j -a Docker
-
-set SLEEP_COMMAND "sleep 50"
 
 tmux new-window   -d -n "services" "cd ~/Projects/lusi/services/cassandra && $SLEEP_COMMAND && docker compose up; fish"
 tmux split-window -h -t "services" "cd ~/Projects/lusi/services/rabbitMQ  && $SLEEP_COMMAND && docker compose up; fish"
@@ -26,9 +21,17 @@ tmux split-window -h -t "services" "cd ~/Projects/lusi/services/minio     && $SL
 tmux split-window -v -t "services" "cd ~/Projects/lasi/                   && fish"
 tmux split-window -h -t "services" "cd ~/Projects/lusi/                   && fish"
 tmux split-window -h -t "services" "cd ~/Projects/levi/                   && fish"
-tmux split-window -v -t "services" "cd ~/Projects/lui/                    && fish"
+tmux split-window -v -t "services" "cd ~/Projects/luigui/                 && fish"
 tmux split-window -h -t "services" "cd ~/Projects/                        && fish"
 tmux split-window -h -t "services" "cd ~/Projects/                        && fish"
 tmux select-layout   -t "services" tiled
+
+tmux new-window -d -n "levi"      "cd ~/Projects/levi/ && fish"
+
+tmux new-window -d -n "lusi"      "cd ~/Projects/lusi/ && fish"
+
+tmux new-window -d -n "luilibs"   "cd ~/Projects/luilibs/ && fish"
+
+tmux new-window -d -n "luigui"    "cd ~/Projects/luigui/ && fish"
 
 bw copy passwordtotp "Lavego Login"
--- work.nu
+++ work.nu
@@ -6,7 +6,7 @@ let current_session = (tmux display-message -p '#S')
 let shell_cmd = "nu"
 let sleep_cmd = "sleep 50sec"
 
-if (do -i { tmux switch-client -t $session_name } | complete | get exit_code | into bool | not $in) {
+if (do -i { tmux switch-client -t $session_name } | complete | get stderr | is-empty) {
   tmux kill-session -t $current_session
   exit 0
 }
@@ -17,9 +17,9 @@ tmux rename-window "main"
 # launch docker in the background
 ^open -g -j -a Docker
 
-tmux new-window   -d -n $container_pane_name ("cd ~/Projects/lusi/; " + $sleep_cmd + "; yarn docker:cassandra:up; " + $shell_cmd)
-tmux split-window -h -t $container_pane_name ("cd ~/Projects/lusi/; " + $sleep_cmd + "; yarn docker:rabbitmq:up; " + $shell_cmd)
-tmux split-window -h -t $container_pane_name ("cd ~/Projects/lusi/; " + $sleep_cmd + "; yarn docker:minio:up; " + $shell_cmd)
+tmux new-window   -d -n $container_pane_name ("cd ~/Projects/lusi/; " + $sleep_cmd + "; do -i { yarn docker:cassandra:up; }; " + $shell_cmd)
+tmux split-window -h -t $container_pane_name ("cd ~/Projects/lusi/; " + $sleep_cmd + "; do -i { yarn docker:rabbitmq:up; }; " + $shell_cmd)
+tmux split-window -h -t $container_pane_name ("cd ~/Projects/lusi/; " + $sleep_cmd + "; do -i { yarn docker:minio:up; }; " + $shell_cmd)
 tmux split-window -v -t $container_pane_name ("cd ~/Projects/lasi/; " + $shell_cmd)
 tmux split-window -h -t $container_pane_name ("cd ~/Projects/lusi/; " + $shell_cmd)
 tmux split-window -h -t $container_pane_name ("cd ~/Projects/levi/; " + $shell_cmd)
